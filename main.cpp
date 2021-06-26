2e895999c8342441a37aa6e75c1b6aac8d96137d
Findereal <armand.lebrec@gmail.com>
2021-06-26 17:36:32 +0200

    Source code ok, mauvais link à la compilation cmake

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by armand on 26/06/2021.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    // glfw needs to be initialized and terminated
+    glfwInit();
+
+    // declare version of OpenGL, here 3.3 (version A.B --> MAJOR = A, MINOR = B)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // declare profile of OpenGL
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // creates window object with error detection.
+    // a window has to be deleted after use
+    GLFWwindow *window = glfwCreateWindow(600, 600, "title", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // use the window
+    glfwMakeContextCurrent(window);
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by armand on 26/06/2021.
+//
+
+#ifndef OPENGLTUTO_MAIN_H
+#define OPENGLTUTO_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //OPENGLTUTO_MAIN_H
